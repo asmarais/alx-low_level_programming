09422bbb162a305c984e264a2e6f92347e5c0d20
asmarais <94301506+asmarais@users.noreply.github.com>
2023-03-20 14:24:35 +0100

    fourth task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	char ch;
-	
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		putchar(ch);
-	
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
 	putchar('\n');
-	
+
 	return (0);
 }
