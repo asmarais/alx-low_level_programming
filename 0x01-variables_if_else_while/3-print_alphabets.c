432c34a3ebbea2882f3cf12510d889fb29bfee0d
asma <rais.asma99@gmail.com>
2023-03-20 14:00:49 +0100

    fourth task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - print the alphabets in lower case and in uppercase. 
+ *Return: 0.
+ */
+
+int main(){
+	char ch;
+	for(ch='a'; ch<='z'; ch++)
+		putchar(ch);
+	
+	for(ch='A'; ch<='Z'; ch++)
+		putchar(ch);
+	
+	putchar("\n");
+	
+	return 0;
+}
