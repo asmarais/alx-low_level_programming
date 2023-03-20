38ab662115294f88c705b77138d11dcf7e97e70e
asmarais <94301506+asmarais@users.noreply.github.com>
2023-03-20 14:52:44 +0100

    6th task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the number from 0 to 9.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int a;
+
+for (a = 0; a <10; a++)
+	printf("%d",a);
+	printf('\n');
+
+	return (0);
+}
