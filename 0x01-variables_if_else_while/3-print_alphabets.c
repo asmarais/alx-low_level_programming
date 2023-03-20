03e04db55ed132593a5d669b1a31e47a7e41f2e7
asma <rais.asma99@gmail.com>
2023-03-20 14:08:56 +0100

    fourth task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,14 @@
  *Return: 0.
  */
 
-int main()
+int main(void)
 {
 	char ch;
+	
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
 	putchar('\n');
-	return 0;
+	return (0);
 }
