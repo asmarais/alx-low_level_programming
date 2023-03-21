8b3c0f9971371db3bd153be835ea28b80277ecc0
asma <rais.asma99@gmail.com>
2023-03-21 12:42:17 +0100

    24_hours + documentation

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *jack_baue - print all the hours and minutes of the day
+ *
+ *Return : void
+ */
+
+void jack_bauer(void)
+{
+	for (c = 0; c <= 2; c++)
+	{
+		for (k =0; k <=9; k++)
+		{
+			if ((c <= 1) || (c == 2 && k <= 3))
+			{
+				for (q = 0; q <= 5; q++)
+
+				{
+					for(p = 0; p <= 9; p++)
+					{
+						_putchar('c');
+						_putchar('k');
+						_putchar(':');
+						_putchar('q');
+						_putchar('p');
+					}
+				}
+			}
+		}
+	}
+}
