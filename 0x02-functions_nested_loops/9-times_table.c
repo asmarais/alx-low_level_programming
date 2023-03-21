459bdfadfb6a0b64def58cb8e66ae890629e40b1
asmarais <94301506+asmarais@users.noreply.github.com>
2023-03-21 15:08:59 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,34 @@
 
 void times_table(void)
 {
-	int a, b;
+	int a, b, c, u, z;
 
-	for (a = 0; a < 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b < 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			_putchar('c');
-			_putchar(',');
+			if (c > 9)
+            {
+                u = c % 10;
+                z = (c-u) / 10;
+                _putchar(',');
+                _putchar(' ');
+                _putchar(u + '0');
+                _putchar(z + '0');
+            }
+            else
+            {
+                if (b != 0)
+            {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+            }
+                _putchar(c + '0');
+            }
 		}
-		c = a * b;
-		_putchar('c');
+
+            _putchar('\n');
 	}
 }
