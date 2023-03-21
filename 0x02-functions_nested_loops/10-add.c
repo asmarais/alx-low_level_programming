85ba6c0699992f52fdf48940fccd552fded7872a
asma <rais.asma99@gmail.com>
2023-03-21 15:51:34 +0100

    functions + documentation

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *add - prints the 9 times table, starting with 0.
+ *@a: the first number
+ *@b: the second number
+ *
+ *Return: the sum of a and b.
+ */
+
+int add(int a, int b)
+{
+	int s;
+
+	s = a + b;
+	return (s);
+}
