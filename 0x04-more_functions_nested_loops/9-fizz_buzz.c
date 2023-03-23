0562bc5d295cd805a9852d4e5c898285d2b0667a
asmarais <94301506+asmarais@users.noreply.github.com>
2023-03-23 17:14:25 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,18 +15,22 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 			printf("%d ", i);
+		if (x != 100)
+		{
+			printf(" ");
+		}
 	}
 	printf('\n');
 	return (0);
