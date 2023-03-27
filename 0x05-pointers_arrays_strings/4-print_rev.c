400038d465c6a4578a5aa4809a556be7dee9d033
asma <rais.asma99@gmail.com>
2023-03-27 08:11:02 +0200

    5th task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,17 @@
 
 void print_rev(char *s)
 {
-	int len;
+	int len = 0;
 
-	len = _strlen(s);
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
 	while (len >= 0)
 	{
-		_putchar(*(s + len));
+		_putchar(*(s--));
 		len--;
 	}
 	_putchar('\n');
