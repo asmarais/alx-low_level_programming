94a318a87c2152b5e535ff204ca7c20de0f789c1
asma <rais.asma99@gmail.com>
2023-03-27 08:06:23 +0200

    5th task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: a pointer to the first character
+ * Return: void.
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
+	_putchar('\n');
+}
