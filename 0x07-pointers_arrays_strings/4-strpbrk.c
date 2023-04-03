844b02013f9a86f2fc5ee4a21f760d3b6ed58d3a
asma <rais.asma99@gmail.com>
2023-04-03 14:25:24 +0200

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: a string
+ * @accept: a string
+ * Return: a string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *temp = s;
+	char *temp1;
+
+	while (*temp != '\0')
+	{
+		temp = accept;
+		while (*temp1 != '\0')
+		{
+			if (*temp == *temp1)
+				return (temp);
+			temp1++;
+		}
+		temp++;
+	}
+	return (NULL);
+}
