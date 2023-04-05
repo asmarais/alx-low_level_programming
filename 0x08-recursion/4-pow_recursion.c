9f0b81b0164d0558fdc97f4c82f0ac8fc7b12403
asmarais <rais.asma99@gmail.com>
2023-04-05 13:07:22 -0700

    pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - a function that returns the value of x
+ * raised to the power of y
+ * @x: an integer
+ * @y: an integer
+ * Return: pow value.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+
+	return (-1);
+}
