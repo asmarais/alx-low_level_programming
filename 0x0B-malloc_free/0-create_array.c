1a1f6e300d2b4ec08febe3340363b607fb76a186
asmarais <94301506+asmarais@users.noreply.github.com>
2023-04-11 22:14:52 +0100

    first task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,11 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 	arr = malloc(size * sizeof(char));
-
+	if (arr == NULL)
+		return (NULL);
 	i = 0;
 	while (i < size)
 	{
