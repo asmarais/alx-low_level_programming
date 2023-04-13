edff9ea55e4e79d9170378d86941b5961ed5ab0a
asmarais <rais.asma99@gmail.com>
2023-04-13 08:13:01 -0700

    third task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc -  allocates memory for an array.
+ * @nmemb: unsigned int
+ * @size: unsigned int
+ * Return: pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *arr;
+	unsigned int count = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	while (count < nmemb * size)
+	{
+		arr[count] = 0;
+		count++;
+	}
+	return (count);
+}
