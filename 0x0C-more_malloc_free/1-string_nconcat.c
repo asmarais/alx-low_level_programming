90e3619cb90e1a65169b09f56521e9082e4358d5
asmarais <94301506+asmarais@users.noreply.github.com>
2023-04-13 15:43:21 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (arr == NULL)
 		return (NULL);
 	
-	for (count = 0; count < len1; count++)
+	for (count = 0; count < size1; count++)
 	{
 		arr[count] = s1[count];
 	}
