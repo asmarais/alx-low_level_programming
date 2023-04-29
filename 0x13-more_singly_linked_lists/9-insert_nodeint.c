efae64f49a0393db476b9f7a4040717b08a30856
asmarais <rais.asma99@gmail.com>
2023-04-29 15:53:23 -0700

    project 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int nodes = 0, i = 0;
 	listint_t *temp, *node;
 
+	if (!head)
+		return (NULL);
 	temp = *head;
 	while (temp)
 	{
