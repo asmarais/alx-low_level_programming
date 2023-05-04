db74cf28fccacc1af0d21055258f8eeb5666a9d0
asmarais <rais.asma99@gmail.com>
2023-05-04 04:38:41 -0700

    project 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - A function that prints a binary number without % or /
+ * @n: The number to be printed
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int max = 1;
+
+	if (n > 1)
+		print_binary(n >> 1);
+
+     	if (n & max)
+		putchar('1');
+	else
+		putchar('0');
+}
