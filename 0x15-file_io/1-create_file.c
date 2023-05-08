5803d583bb81f948d5d8f08ae77e595e0756a5db
asmarais <rais.asma99@gmail.com>
2023-05-08 10:37:10 -0700

    project 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - Create a function that creates a file
+ * @filename:  the name of the file to create
+ * @text_content: the text to be printed
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, w;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (o == -1)
+		return (-1);
+	while (text_content && *(text_content + len))
+		len++;
+	w = write(o, text_content, len);
+	close(o);
+	if (w == -1)
+		return (-1);
+	return (1);
+}
