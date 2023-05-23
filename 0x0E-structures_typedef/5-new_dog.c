b5a927fe67e2444dfb2f3d021398dcdb1be8c9d8
asmarais <rais.asma99@gmail.com>
2023-05-23 08:21:35 -0700

    hey

---
+++ 0x0E-structures_typedef/5-new_dog.c
@@ -0,0 +1,48 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog -  a function that creates a new dog.
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: returns a pointer to newly created dog structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *cpname, *cpowner;
+	int lenname = 0, lenowner = 0, i;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	while (name[lenname])
+		lenname++;
+	while (owner[lenowner])
+		lenowner++;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	cpname = malloc(lenname + 1);
+	if (cpname == NULL)
+		return (NULL);
+
+	for (i = 0; i < name[i]; i++)
+		cpname[i] = name[i];
+	cpname[i] = '\0';
+
+	cpowner = malloc(lenowner + 1);
+	if (cpowner == NULL)
+		return (NULL);
+
+	for (i = 0; i < owner[i]; i++)
+		cpowner[i] = owner[i];
+	cpowner[i] = '\0';
+
+	new_dog->name = cpname;
+	new_dog->age = age;
+	new_dog->owner = cpowner;
+	return (new_dog);
+}
---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(NULL, 2, "Jay", "Django");
+    return (0);
+}
