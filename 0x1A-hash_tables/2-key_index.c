473f8deda0a1fdf0dd60783e042cd9e2c93e4555
asmarais <rais.asma99@gmail.com>
2023-07-06 07:11:21 -0700

    hash tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Write a function that gives you the index of a key.
+ * @heky: the key
+ * @size: the size of the array of the hash table
+ * Return:Returns the index at which the key/value pair should
+ * be stored in the array of the hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{}
