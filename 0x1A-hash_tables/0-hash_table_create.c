c0df995cb70572cdba98fb3b7466ab8e3efdcf7d
asmarais <94301506+asmarais@users.noreply.github.com>
2023-07-07 11:33:39 +0100

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash;
 	unsigned long int i;
 
-	hash = malloc(sizeof(hash_table_t));
+	hash = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (hash == NULL)
 		return (NULL);
 
