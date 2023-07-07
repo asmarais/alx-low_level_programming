fbcacd55f9e6ea0c8ffdf42cf4baae5ccd8543ed
asmarais <rais.asma99@gmail.com>
2023-07-07 05:24:54 -0700

    project 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - a function that adds an element to the hash table
+ * @ht: the hash table
+ * @key: the key
+ * @value: the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *node, *new;
+	char *cp_value;
+
+	if (key == NULL || *key == '\0' || ht == NULL || value == NULL)
+		return (0);
+
+	cp_value = strdup(value);
+	if (cp_value == NULL)
+		return (0);
+	idx = key_index((const unsigned char *)key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	node = ht->array[idx];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = cp_value;
+			return (1);
+		}
+	}
+	new->key = strdup(key);
+	new->value = cp_value;
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+	return (1);
+}
