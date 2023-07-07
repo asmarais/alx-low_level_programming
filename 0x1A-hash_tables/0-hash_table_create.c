fb034cdee8279aa8aa1c14981e1e2dd326c365b1
asmarais <rais.asma99@gmail.com>
2023-07-07 03:28:08 -0700

    project 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash;
+	unsigned long int i;
 
 	hash = malloc(sizeof(hash_table_t));
 	if (hash == NULL)
@@ -21,5 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	hash->size = size;
+	for (i = 0; i < size; i++)
+		hash->array = NULL;
 	return (hash);
 }
