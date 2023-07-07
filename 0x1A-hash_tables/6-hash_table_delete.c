01b9ff701c545f4b3ba98c540406a68835451b6c
asmarais <94301506+asmarais@users.noreply.github.com>
2023-07-07 15:29:14 +0100

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,21 +11,16 @@ void hash_table_delete(hash_table_t *ht)
 
 	for (i = 0; i < ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		node = ht->array[i];
+		while (node != NULL)
 		{
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				tmp = node;
-				node = node->next;
-				free(tmp->key);
-				free(tmp->value);
-				free(tmp);
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
 			}
-		}
 	}
-	free(node);
-	free(temp);
 	free(ht->array);
 	free(ht);
 }
