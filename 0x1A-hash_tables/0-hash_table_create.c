fe04fa5f7f2e2606b327badb03e3e3862b7524c0
asmarais <rais.asma99@gmail.com>
2023-07-07 03:21:36 -0700

    project 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,22 +9,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash;
-	unsigned long int i;
 
 	hash = malloc(sizeof(hash_table_t));
 	if (hash == NULL)
 		return (NULL);
 
-	hash->array = malloc(sizeof(hash_node_t) * size);
+	hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash->array == NULL)
 	{
 		free(hash);
 		return (NULL);
 	}
 	hash->size = size;
-	for (i = 0; i < size; i++)
-	{
-		hash->array[i] = NULL;
-	}
 	return (hash);
 }
