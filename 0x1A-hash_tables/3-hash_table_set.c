2ca39ae8f718777ebaec1bc374756f77c511a796
asmarais <94301506+asmarais@users.noreply.github.com>
2023-07-07 13:28:37 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *new;
 	char *cp_value;
 
-	if (key == NULL || *key == '\0' || ht == NULL || value == NULL || !ht->array)
+	if (key == NULL || *key == '\0' || ht == NULL || value == NULL)
 		return (0);
 
 	cp_value = strdup(value);
