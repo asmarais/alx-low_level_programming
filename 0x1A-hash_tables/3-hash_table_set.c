523ce8798b024a10feaf2074379346e765cc6eb9
asmarais <94301506+asmarais@users.noreply.github.com>
2023-07-07 15:53:05 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,11 +36,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
+		free(value_copy);
 		return (0);
 	}
 	new->key = strdup(key);
 	if (new->key == NULL)
 	{
+		free(value_copy);
 		free(new);
 		return (0);
 	}
