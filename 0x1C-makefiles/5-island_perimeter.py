a0acf13666d27917f7de7c3a3a49087e72bd8f35
asmarais <94301506+asmarais@users.noreply.github.com>
2023-08-23 16:06:30 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,18 +2,18 @@
 
 
 def island_perimeter(grid):
-    """Python function to return the perimeter of an island"""
+    """Python function to return the perimeter of an island in  GRID"""
 
     p = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if i + 1 == len(grid) or grid[i + 1][j] == 0:
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if x + 1 == len(grid) or grid[x + 1][y] == 0:
                     p += 1
-                if j + 1 == len(grid[0]) or grid[i][j + 1] == 0:
+                if y + 1 == len(grid[0]) or grid[x][y + 1] == 0:
                     p += 1
-                if i - 1 == -1 or grid[i - 1][j] == 0:
+                if x - 1 == -1 or grid[x - 1][y] == 0:
                     p += 1
-                if j - 1 == -1 or grid[i][j - 1] == 0:
+                if y - 1 == -1 or grid[x][y - 1] == 0:
                     p += 1
     return(p)
