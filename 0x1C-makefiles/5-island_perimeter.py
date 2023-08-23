25c7d34cd5c8081a2b84b04456ac50dc9fa22168
asmarais <rais.asma99@gmail.com>
2023-08-23 15:43:50 +0100

    makefile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """Python function to return the perimeter of an island"""
+    
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i + 1 == len(grid) or grid[i+1][j] == 0:
+                    p += 1
+                if j + 1 == len(grid[0]) or grid[i][j + 1] == 0:
+                    p += 1
+                if i - 1 == -1 or grid[i - 1][j] == 0:
+                    p += 1
+                if j - 1 == -1 or grid[i][j - 1] == 0:
+                    p += 1
+    return (p)
