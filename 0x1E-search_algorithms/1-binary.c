534013160842a21b7863f34a0b32fb4b3097f3a0
asmarais <rais.asma99@gmail.com>
2023-10-11 07:01:23 +0100

    search algorith advanced

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,7 +2,7 @@
 
 /**
  *binary_search -  a function that searches for a value
- *in an array of integers using the Linear search algorithm
+ *in an array of integers using the Binary search algorithm
  *@array:  a pointer to the first element of the array to search in
  *@size: the number of elements in array
  *@value: the value to search for
